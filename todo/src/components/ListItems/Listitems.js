9420fed990ef8ab4013caf5c064aae5def414368
Rahul Pachauri <rpachauri584@gmail.com>
2023-08-06 16:37:31 +0530

    add respnsiveness

--- todo/src/components/ListItems/Listitems.js
+++ todo/src/components/ListItems/Listitems.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRectangleXmark } from '@fortawesome/free-solid-svg-icons';
-import { ListContext } from '../AddItems/AddItems';
+
 import './Listitems.css'
 
 function Listitems(props) {
